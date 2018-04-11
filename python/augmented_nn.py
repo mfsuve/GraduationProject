11e419621600cf688a0f852911524212da866cb7
mfsuve <suvemustafa@gmail.com>
2018-04-12 01:05:07 +0300

    Attempt to siamese network

--- python/augmented_nn.py
+++ python/augmented_nn.py
@@ -4,10 +4,11 @@ import pickle
 from matplotlib import pyplot as plt
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, Input, ZeroPadding2D, ThresholdedReLU, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, Input, ZeroPadding2D, ThresholdedReLU, BatchNormalization, merge
 from keras import backend as K
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
 
 names = ['hayvan', 'sayitut', 'sefiller', 'sokrates', 'sultan']
 num_test_classes = len(names)
@@ -20,7 +21,7 @@ def load_data():
 	test_path = 'finals/reduced/test/'
 	train_img = []
 	test_img = []
-	num_scrapped_book = 15
+	num_scrapped_book = 5
 	# Collect initials
 	for name in names:
 #		train_img.append(cv2.resize(cv2.imread(train_path + name + '1.png'), (100, 150), interpolation=cv2.INTER_CUBIC))
@@ -92,23 +93,22 @@ def create_model_vgg16():
 		layer.trainable = False
 
 	top_model = Sequential()
-	top_model.add(ZeroPadding2D((4, 4), input_shape=base_model.output_shape[1:]))
-	top_model.add(Conv2D(32, (9, 9), activation='relu'))
-	top_model.add(MaxPooling2D(pool_size=(4, 4)))
+	top_model.add(ZeroPadding2D((1, 1), input_shape=base_model.output_shape[1:]))
+	top_model.add(Conv2D(32, (3, 3), activation='relu'))
 	top_model.add(BatchNormalization())
 	top_model.add(ThresholdedReLU(0))
 
-	top_model.add(ZeroPadding2D((4, 4)))
-	top_model.add(Conv2D(32, (9, 9), activation='relu'))
-	top_model.add(MaxPooling2D(pool_size=(4, 4)))
+	top_model.add(ZeroPadding2D((1, 1)))
+	top_model.add(Conv2D(32, (3, 3), activation='relu'))
 	top_model.add(BatchNormalization())
 	top_model.add(ThresholdedReLU(0))
 
-	top_model.add(ZeroPadding2D((4, 4)))
-	top_model.add(Conv2D(32, (9, 9), activation='relu'))
-	top_model.add(MaxPooling2D(pool_size=(4, 4)))
+	top_model.add(ZeroPadding2D((1, 1)))
+	top_model.add(Conv2D(32, (3, 3), activation='relu'))
 	top_model.add(BatchNormalization())
 	top_model.add(ThresholdedReLU(0))
+
+	top_model.add(MaxPooling2D(pool_size=(2, 2)))
 
 	top_model.add(Flatten())
 	top_model.add(BatchNormalization())
@@ -117,6 +117,21 @@ def create_model_vgg16():
 	m = Model(inputs=base_model.input, outputs=top_model(base_model.output))
 
 	return m
+
+
+def siamese_batch(X, datagen, batch_size=32):
+	cls_num = X.shape[0]
+	categories = np.random.choice(cls_num, size=(batch_size,), replace=False)
+	pairs = [np.zeros((batch_size, 150, 100, 3)) for i in range(2)]
+	targets = np.zeros((batch_size,))
+	targets[batch_size//2:] = 1
+
+	for i in range(batch_size):
+		category = categories[i]
+		pairs[0][i, :, :, :] = datagen.random_transform(X[category])
+		category_2 = category if i >= batch_size // 2 else (category + np.random.randint(1, cls_num)) % cls_num
+		pairs[1][i, :, :, :] = datagen.random_transform(X[category_2])
+	return pairs, targets
 
 
 def augmentation_fit():
@@ -132,9 +147,32 @@ def augmentation_fit():
 		fill_mode='constant')  # Constant zero
 
 	train_datagen.fit(X_train_3ch)
-	train_generator = train_datagen.flow(X_train_3ch, Y_train, batch_size=32)
+	losses = []
 
-	return model.fit_generator(train_generator, steps_per_epoch=30, epochs=200, validation_data=(X_test_3ch, Y_test))
+	for i in range(100):
+		(inputs, targets) = siamese_batch(X_train_3ch, train_datagen, 9)
+
+		for g in range(9):
+			plt.suptitle('Same' if targets[g] == 1 else 'Different')
+			plt.subplot(1, 2, 1)
+			plt.imshow(inputs[0][g, :, :, ::-1])
+
+			plt.subplot(1, 2, 2)
+			plt.imshow(inputs[1][g, :, :, ::-1])
+			plt.show()
+
+		loss = model.train_on_batch(inputs, targets)
+
+		### TODO: Calculate Accuracy Here !!!
+
+		losses.append(loss)
+		print('iteration {},\ttraining loss: {}'.format(i, loss))
+
+	return {'loss': losses, 'acc': [], 'val_loss': [], 'val_acc': []}
+
+	# train_generator = train_datagen.flow(X_train_3ch, Y_train, batch_size=32)
+
+	# return model.fit_generator(train_generator, steps_per_epoch=30, epochs=200, validation_data=(X_test_3ch, Y_test))
 
 
 def normal_fit():
@@ -143,7 +181,23 @@ def normal_fit():
 	return model.fit(X_train_3ch, Y_train, batch_size=32, epochs=600, validation_data=(X_test_3ch, Y_test))
 
 
-def run(lr=0.001, augmented=True, modelno=3):  # If modelno changes, change the model_name
+def siamese(smodel):
+	input_shape = (150, 100, 3)
+	left_input = Input(input_shape)
+	right_input = Input(input_shape)
+
+	encoded_l = smodel(left_input)
+	encoded_r = smodel(right_input)
+
+	L1 = lambda x: K.abs(x[0] - x[1])
+	both = merge([encoded_l, encoded_r], mode=L1, output_shape=lambda x: x[0])
+
+	prediction = Dense(1, activation='softmax')(both)
+
+	return Model(inputs=[left_input, right_input], outputs=prediction)
+
+
+def run(lr=0.001, augmented=True, modelno=3):  # If modelno changes, change the model_name (vgg16 part)
 	global model, X_train_3ch, X_test_3ch, Y_train, Y_test
 	# Load images
 	(X_train, y_train), (X_test, y_test) = load_data()
@@ -164,8 +218,16 @@ def run(lr=0.001, augmented=True, modelno=3):  # If modelno changes, change the 
 		model = create_model2()
 	else:  # default
 		model = create_model_vgg16()
-	model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-	K.set_value(model.optimizer.lr, lr)  # it was 0.01
+
+	model = siamese(model)
+
+	sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+	model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+	# Because of Siamese, I use the one above
+	# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+	# K.set_value(model.optimizer.lr, lr)  # it was 0.01
+
+	print('laerning rate is', K.get_value(model.optimizer.lr))
 
 	if augmented:
 		history = augmentation_fit()
