dbad3fc7be338f2c1ce7c1fb024ac04cd25e9764
mfsuve <suvemustafa@gmail.com>
2018-04-19 08:30:40 +0300

    Showed only two plots side by side

--- python/history_test/show.py
+++ python/history_test/show.py
@@ -6,25 +6,36 @@ def show_plot(history, title=None):
 	else:
 		plt.suptitle(title)
 
-	plt.subplot(221)
-	plt.ylim(0, 1.05)
-	plt.title('acc')
-	plt.plot(history['acc'])
+	try:
+		plt.subplot(221)
+		plt.ylim(0, 1.05)
+		plt.title('acc')
+		plt.plot(history['acc'])
 
-	plt.subplot(222)
-	plt.ylim(0, 1.05)
-	plt.title('val_acc')
-	plt.plot(history['val_acc'])
+		plt.subplot(222)
+		plt.ylim(0, 1.05)
+		plt.title('val_acc')
+		plt.plot(history['val_acc'])
 
-	plt.subplot(223)
-	plt.ylim(0, 10)
-	plt.title('loss')
-	plt.plot(history['loss'])
+		plt.subplot(223)
+		plt.ylim(0, 10)
+		plt.title('loss')
+		plt.plot(history['loss'])
 
-	plt.subplot(224)
-	plt.ylim(0, 10)
-	plt.title('val_loss')
-	plt.plot(history['val_loss'])
+		plt.subplot(224)
+		plt.ylim(0, 10)
+		plt.title('val_loss')
+		plt.plot(history['val_loss'])
+	except KeyError:
+		plt.subplot(121)
+		plt.ylim(0, 1.05)
+		plt.title('acc')
+		plt.plot(history['acc'])
+
+		plt.subplot(122)
+		plt.ylim(0, 10)
+		plt.title('loss')
+		plt.plot(history['loss'])
 
 	figManager = plt.get_current_fig_manager()
 	figManager.window.showMaximized()
